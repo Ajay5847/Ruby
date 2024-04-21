1f1898df2ba3ef28e9be42905f2ce7f78dc63222
Ajay Kumar M <ajaybunty91@gmail.com>
2024-04-21 23:22:52 +0530

    Astreik in regular expression

--- Chapter-1/s3_strings.rb
+++ Chapter-1/s3_strings.rb
@@ -20,5 +20,22 @@ str[str.size - 1] == str[-1] # both are same !
 
 The regular expression . will match any single char and .. will match two characters
 A. will match for An, Ar, A=, Au....etc and A.. will match for Adh, Aju,... etc
+
+# period (.)
 We can use backslash to turn of meaning of special characters => .r\. will match for Dr., Ar., Sr.,...etc
 
+\d => for digitis
+\w => for will match any letter, number or the underscore.
+\s => for will match any white space character including the vanilla space, the tab, and the newline.
+
+03:14 AM => regualr expression to search is (\d\d:\d\d (AM|PM))
+
+# Astriek (*)
+
+an asterisk (*) matches zero or more of the thing that came just before it.
+AB* will match AB, A, ABBBBBBBBBBBBBBBB
+.* will match any number of characters 
+
+• The regular expression George.* will match the full name of anyone whose first name is George.
+• In contrast, .*George will match the name of anyone whose last name is George.
+• Finally .*George.* will match the name of anyone who has George in his name somewhere.
